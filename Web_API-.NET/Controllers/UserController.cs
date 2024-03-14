05d3158fb772a8b8013506728c53734e449bcdac
roroduJUDO <rorodujudo@gmail.com>
2024-03-15 00:18:13 +0100

    api adding +docker nearly done

---
+++ Web_API-.NET/Controllers/UserController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDevSysOps.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiDevSysOps.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DevsysopsContext _dbContext;
+
+        public UserController(DevsysopsContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+            var Users = new List<User>();
+            foreach (var user in users)
+            {
+                Users.Add(new User
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.Username,
+                    Password = user.Password,
+                    EnrollmentDate = user.EnrollmentDate
+                });
+            }
+            return Ok(Users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var User = new User
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.Username,
+                Password = user.Password,
+                EnrollmentDate = user.EnrollmentDate
+            };
+            return Ok(User);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User User)
+        {
+            var user = new User
+            {   
+                FirstName = User.FirstName,
+                LastName = User.LastName,
+                Username = User.Username,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, User);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User User)
+        {
+            if (id != User.Id)
+            {
+                return BadRequest();
+            }
+            var user = await _dbContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.FirstName = User.FirstName;
+            user.LastName = User.LastName;
+            user.Username = User.Username;
+            user.Password = User.Password;
+            user.EnrollmentDate = User.EnrollmentDate;
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Users.Remove(user);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Web_API-.NET/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDevSysOps.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public DateTime EnrollmentDate { get; set; }
+}
